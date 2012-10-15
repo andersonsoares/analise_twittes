48ddd7408fc5d52e8c46287dd27957a4e90150c0
Anderson Soares <aersandersonsoares@gmail.com>
2012-10-15 11:54:36 -0300

    creating app structure

---
+++ src/main/java/utils/Conexao.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+	private static Connection connection;
+	
+	public static void criarConexao() throws SQLException {
+		connection = DriverManager.getConnection
+			(
+				//driver + database
+				"jdbc:postgresql://127.0.0.1:5432/monitor_twitter_bkp",
+				//username
+				"aers",
+				//password
+				""
+			);
+	}
+	
+	public static void fecharConexao() throws SQLException {
+		connection.close();
+	}
+	
+}
